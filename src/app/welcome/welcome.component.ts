1f014bc837541be063c5b6da0e1e8f08bb6a6c25
Earnest <earnestcpy@gmail.com>
2019-01-06 17:43:13 -0800

    add comments

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
 
+// @{Decorator}
 @Component({
+  // {attribute}
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
 
+  globalmessage= 'Welcome message from WelcomeComponent global';
+  
   constructor() { }
 
   // void init(){}
   ngOnInit() {
     // ngOnInit(): void     {function(): return type}
+    var localmessage:string = 'Welcome message from WelcomeComponent local';
+    console.log(this.globalmessage);
+    console.log(localmessage);
   }
 
 }
