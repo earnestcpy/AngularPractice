b3d16b32b94bf3ade63ced2cab800e0e7d802071
Earnest <earnestcpy@gmail.com>
2019-01-06 16:50:30 -0800

    add comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root', /*  Create a new component or tag named "app-root" */
-  templateUrl: './app.component.html', /* HTML  template for app-root */
-  styleUrls: ['./app.component.css'] /* Style // CSS for app-root */
+  templateUrl: './app.component.html', /* HTML template for app-root. Indicate the location of HTML template for this specific component */
+  //template: '<h1>{{title}}</h1>',   template =/= templateUrl
+  styleUrls: ['./app.component.css'] /* Style // CSS for app-root.  Indicate the location of HTML template for this specific component */
 })
 export class AppComponent {
   title = 'todo';
