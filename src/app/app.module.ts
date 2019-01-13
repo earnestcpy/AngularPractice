843277d1bd1bdba383e4b62ec148e4a4375d2cf4
Earnest <earnestcpy@gmail.com>
2019-01-13 15:38:08 -0800

    Add validation to login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';    //Angular Module
+import { NgModule } from '@angular/core';                     //Angular Module
+import { FormsModule } from '@angular/forms';                 //Angular Module
 
-import { AppComponent } from './app.component';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';               //Angular Component 
+import { WelcomeComponent } from './welcome/welcome.component'; //Angular Component
+import { LoginComponent } from './login/login.component';//Angular Component
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username= '';
+  password= '';
+  errorMessage= 'Invalid Credential(s)';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  handleLogin(){
+    console.log(this.username  + this.password);
   }
 
 }
