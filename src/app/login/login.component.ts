f570826334310ce63d393cd65c86c1bd67069d7c
Earnest <earnestcpy@gmail.com>
2019-01-17 19:15:06 -0800

    add routing from login to welcome

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+// Inject Router into LoginComponent
+import { Router } from '@angular/router';
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -12,17 +16,22 @@ export class LoginComponent implements OnInit {
   errorMessage= 'Invalid Credential(s)';
   errorMessageInValid = false;
 
-  constructor() { }
+  //Router
+  //Angular.giveMeRouter
+  //Dependency Injection
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
   handleLogin(){
     console.log(this.username  + this.password);
-    if(this.username=== '123'){
+    if(this.username=== '123' && this.password ==='456'){
+      this.router.navigate(['welcome']);
       this.errorMessageInValid= false;
     }
     else{
+      console.log('false');
       this.errorMessageInValid= true; 
     }
   }
