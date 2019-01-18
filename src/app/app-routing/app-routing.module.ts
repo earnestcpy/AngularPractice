9dbd091d979eab8de4837ea8ff868fc2a53e4383
Earnest <earnestcpy@gmail.com>
2019-01-17 19:03:34 -0800

    add routing

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule} from '@angular/router';
+import { WelcomeComponent } from '../welcome/welcome.component';
+import { LoginComponent } from '../login/login.component';
+import { ErrorComponent } from '../error/error.component';
+
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'welcome', component: WelcomeComponent},
+  {path: '**', component: ErrorComponent}      // define unknown or undeclare path and route to Error Component
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { FormsModule } from '@angular/forms';                 //Angular Module
 
 import { AppComponent } from './app.component';               //Angular Component 
 import { WelcomeComponent } from './welcome/welcome.component'; //Angular Component
-import { LoginComponent } from './login/login.component';//Angular Component
+import { LoginComponent } from './login/login.component'; //Angular Component
+import { ErrorComponent } from './error/error.component';//Angular Component
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    LoginComponent
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
